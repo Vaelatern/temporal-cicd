54f492229c64064b75bef13bfadf3b3d49b40c86
Toyam Cox <Vaelatern@voidlinux.org>
2024-07-01 00:06:37 -0400

    Problem: Workflow the barest of shells
    Solution: Go for making it all compile
    
    We learned we need to understand signals during normal execution better

--- cmd/temporal-cicd/smartsheets.go
+++ cmd/temporal-cicd/smartsheets.go
@@ -7,14 +7,59 @@ import (
 )
 
 type SmartSheetTask struct {
-	Sheet   string
-	Project string
-	Version string
+	Sheet       string
+	Project     string
+	Version     string
+	AllSignoffs bool
 }
 
-// SmartSheetNotify takes a git repo and an upload location, does the build, and then is done.
+// SmartSheetNotify
 func SmartSheetNotify(ctx context.Context, task SmartSheetTask) error {
 	activity.GetLogger(ctx).Info("Injecting a row in SmartSheet")
 
+	return nil
+}
+
+// AddRowToSmartsheet
+func AddRowToSmartsheet(ctx context.Context, task SmartSheetTask) error {
+	activity.GetLogger(ctx).Info("Adding a row to SmartSheet")
+
+	return nil
+}
+
+// SetDetailsInSmartsheet
+func SetDetailsInSmartsheet(ctx context.Context, task SmartSheetTask) error {
+	activity.GetLogger(ctx).Info("Noting details in Smartsheet")
+
+	return nil
+}
+
+// UploadBuildResultsToSmartsheet
+func UploadBuildResultsToSmartsheet(ctx context.Context, task SmartSheetTask) error {
+	activity.GetLogger(ctx).Info("Build and Test Logs in Smartsheet")
+	return nil
+}
+
+// DeclareBuildReadySmartsheet
+func DeclareBuildReadySmartsheet(ctx context.Context, task SmartSheetTask) error {
+	activity.GetLogger(ctx).Info("Build ready to deploy, noting so in Smartsheet")
+	return nil
+}
+
+// MarkRowTamperedSmartsheet
+func MarkRowTamperedSmartsheet(ctx context.Context, task SmartSheetTask) error {
+	activity.GetLogger(ctx).Info("Build was tampered with, mark Untrustable")
+	return nil
+}
+
+// DeployToEnv
+func DeployToEnv(ctx context.Context, env_name string) error {
+	activity.GetLogger(ctx).Info("Deploy time")
+	return nil
+}
+
+// DeleteRowFromSmartsheet
+func DeleteRowFromSmartsheet(ctx context.Context, env_name string) error {
+	activity.GetLogger(ctx).Info("Deploy time")
 	return nil
 }
--- cmd/temporal-cicd/workflows.go
+++ cmd/temporal-cicd/workflows.go
@@ -51,6 +51,11 @@ func MakeBuildWorkflow(ctx workflow.Context) error {
 func BuildLifecycleWorkflow(ctx workflow.Context) error {
 	workflow.GetLogger(ctx).Info("We have a new release to handle, and we will from now to the end", "StartTime", workflow.Now(ctx))
 
+	var signal SmartSheetTask
+	end_of_life_timeout := 6 * 31 * 24 * time.Hour
+
+	var tag bool = false
+
 	ao := workflow.ActivityOptions{
 		StartToCloseTimeout: 24 * time.Minute,
 		RetryPolicy: &temporal.RetryPolicy{
@@ -60,35 +65,74 @@ func BuildLifecycleWorkflow(ctx workflow.Context) error {
 	ctx1 := workflow.WithActivityOptions(ctx, ao)
 
 	err := workflow.ExecuteActivity(ctx1, AddRowToSmartsheet).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
 build:
-	err := workflow.ExecuteActivity(ctx1, SetDetailsInSmartsheet).Get(ctx, nil)
-	err := workflow.ExecuteActivity(ctx1, Build).Get(ctx, nil)
-	err := workflow.ExecuteActivity(ctx1, Test).Get(ctx, nil)
-	err := workflow.ExecuteActivity(ctx1, UploadBuildResultsToSmartsheet).Get(ctx, nil)
-	err := workflow.ExecuteActivity(ctx1, DeclareBuildReadySmartsheet).Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx1, SetDetailsInSmartsheet).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+	var build_result error
+	err = workflow.ExecuteChildWorkflow(ctx, MakeBuildWorkflow).Get(ctx, &build_result)
+	if err != nil {
+		return err
+	}
+	err = workflow.ExecuteActivity(ctx1, UploadBuildResultsToSmartsheet).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
+	err = workflow.ExecuteActivity(ctx1, DeclareBuildReadySmartsheet).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
 
 	if tag {
-		for !all_signoffs {
-			wait_for_signoffs
+		for {
+			signalChan := workflow.GetSignalChannel(ctx, "message-from-smartsheet")
+			signalChan.Receive(ctx, &signal)
+			if signal.AllSignoffs {
+				break
+			}
 		}
 		if tag_changed {
 			err := workflow.ExecuteActivity(ctx1, MarkRowTamperedSmartsheet).Get(ctx, nil)
+			if err != nil {
+				return err
+			}
 			goto end_of_life
 
 		}
 		err := workflow.ExecuteActivity(ctx1, DeployToEnv, "prod").Get(ctx, nil)
-		WaitForEndOfLife
+		if err != nil {
+			return err
+		}
 	}
-	err := workflow.ExecuteActivity(ctx1, DeployToEnv, "branch_name").Get(ctx, nil)
+	err = workflow.ExecuteActivity(ctx1, DeployToEnv, "branch_name").Get(ctx, nil)
+	if err != nil {
+		return err
+	}
 
+	for {
+		selector := workflow.NewSelector(ctx)
+		selector.AddReceive(signalChan, func(c workflow.ReceiveChannel, more bool) {
+			c.Receive(ctx, &signal)
+		})
+		selector.Select(ctx)
+	}
 	if signal_branch_changed {
 		goto build
 	}
-	WaitForEndOfLife
 
 end_of_life:
-	Wait6Months
-	err := workflow.ExecuteActivity(ctx1, DeleteRowFromSmartsheet).Get(ctx, nil)
+	err = workflow.Sleep(ctx, end_of_life_timeout) // 6 months normally
+	if err != nil {
+		return err
+	}
+	err = workflow.ExecuteActivity(ctx1, DeleteRowFromSmartsheet).Get(ctx, nil)
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
