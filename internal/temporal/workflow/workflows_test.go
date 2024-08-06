7d4db5140d7c3c27a8c88cc0299b2d602d84a3fe
Toyam Cox <Vaelatern@voidlinux.org>
2024-08-06 00:17:45 -0400

    Problem: Didn't test happy path
    Solution: Add to the workflow test

--- internal/temporal/workflow/workflows_test.go
+++ internal/temporal/workflow/workflows_test.go
@@ -23,9 +23,28 @@ func (s *UnitTestSuite) Test_BuildLifecycleWorkflowGoldenPath() {
 
 	s.True(s.env.IsWorkflowCompleted())
 
-	//err := s.env.GetWorkflowError()
-	//s.Error(err)
-	//var applicationErr *temporal.ApplicationError
-	//s.True(errors.As(err, &applicationErr))
-	//s.Equal("SimpleActivityFailure", applicationErr.Error())
+	err := s.env.GetWorkflowError()
+	s.NoError(err)
+}
+
+func (s *UnitTestSuite) Test_BuildLifecycleWorkflowAlmostGoldenPath() {
+	s.env.OnActivity(activity.AddRowToSmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+	s.env.OnActivity(activity.SetDetailsInSmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+	s.env.OnActivity(activity.MakeBuild, mock.Anything, mock.Anything).Return(activity.BuildResponse{}, nil)
+	s.env.OnActivity(activity.UploadBuildResultsToSmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+	s.env.OnActivity(activity.DeclareBuildReadySmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+	s.env.OnActivity(activity.DeployToEnv, mock.Anything, mock.Anything).Return(activity.DeployEnvResponse{}, nil)
+	s.env.OnActivity(activity.DeleteRowFromSmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+
+	//s.env.OnActivity(activity.SmartSheetNotify, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+	//s.env.OnActivity(activity.MarkRowTamperedSmartsheet, mock.Anything, mock.Anything).Return(activity.SmartSheetTaskResponse{}, nil)
+
+	s.env.RegisterWorkflow(MakeBuildWorkflow)
+
+	s.env.ExecuteWorkflow(BuildLifecycleWorkflow)
+
+	s.True(s.env.IsWorkflowCompleted())
+
+	err := s.env.GetWorkflowError()
+	s.NoError(err)
 }
