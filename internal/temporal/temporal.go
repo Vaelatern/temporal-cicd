37a605919d9d2df902aa2948fa919279d90d957c
Toyam Cox <Vaelatern@voidlinux.org>
2024-11-22 13:39:18 -0500

    Problem: My logic for clients is immature
    Solution: Get variables right way around, enable namespaces and TLS

--- internal/temporal/temporal.go
+++ internal/temporal/temporal.go
@@ -1,6 +1,7 @@
 package temporal
 
 import (
+	"crypto/tls"
 	"log/slog"
 	"os"
 
@@ -17,15 +18,28 @@ func Logger() log.Logger {
 }
 
 func EasyClient(logger log.Logger) (client.Client, error) {
-	temporalHost, useDefault := os.LookupEnv("TEMPORAL_ADDRESS")
-	if useDefault {
+	temporalHost, haveValue := os.LookupEnv("TEMPORAL_ADDRESS")
+	if !haveValue {
 		temporalHost = "localhost:7233"
 	}
+	temporalNamespace, haveValue := os.LookupEnv("TEMPORAL_NAMESPACE")
+	if !haveValue {
+		temporalNamespace = "default"
+	}
+	_, haveValue = os.LookupEnv("TEMPORAL_TLS")
+	var temporalTLS *tls.Config
+	if haveValue {
+		temporalTLS = &tls.Config{}
+	}
+
 	clientOptions := client.Options{
 		HostPort:    temporalHost,
 		Credentials: nil,
 		Logger:      logger,
+		Namespace:   temporalNamespace,
 	}
+	clientOptions.ConnectionOptions.TLS = temporalTLS
+
 	temporalClient, err := client.Dial(clientOptions)
 	return temporalClient, err
 }
